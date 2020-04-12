bafd781b24e93409ac62146788469a2fecbcf072
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-12 08:50:55 +0530

    update

--- mongodb.js
+++ mongodb.js
@@ -1,12 +1,24 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 // database connection
-const URL= "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb";
-const dbName= "react-node3";
+const URL = "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb";
+const dbName = "react-node3";
 let db;
 
 
-MongoClient.connect(URL,(err, client)=>{
+MongoClient.connect(URL, (err, client) => {
     console.log("database connected successfully");
-    db= client.db(dbName);
+    db = client.db(dbName);
 });
+
+exports.fetchData = (collectionName, data) => {
+   
+};
+
+exports.createData = (collectionName, data) => {
+    return new Promise((reject, resolve) => {
+        db.collection(collectionName).insertOne(data)
+            .then(doc => resolve(doc))
+            .catch(err=> reject(err))
+    })
+};
