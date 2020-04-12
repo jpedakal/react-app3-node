81865a9e9449f07c1342e508b5d8dcbbfb24c211
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-12 07:59:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const mongo = require('mongodb');
+const {MongoClient} = require('mongodb');
 const artist= require('./api/artist');
 
+// database connection
 
+// middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// routes
 app.use('/',artist);
 
 const Port = 5000;
