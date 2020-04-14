73b9e0578dcc1fcdfeab8f63f2bc43a3be370b57
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-14 17:06:14 +0530

    update

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 
